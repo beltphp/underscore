d4c7c76152efc1933f17258ec10b262914823576
Ramon Kleiss <ramonkleiss@gmail.com>
2014-10-24 13:05:29 +0200

    Complete the _ class

--- src/_.php
+++ src/_.php
@@ -1,6 +1,8 @@
 <?php
 namespace Belt;
 
+use Symfony\Component\PropertyAccess\PropertyAccess;
+
 /**
  * @author Ramon Kleiss <ramonkleiss@gmail.com>
  */
@@ -12,7 +14,7 @@ class _
     /**
      * @param array
      */
-    public function __construct(array $container)
+    public function __construct(array $container = array())
     {
         $this->container = $container;
     }
@@ -22,7 +24,7 @@ class _
      *
      * @return _
      */
-    public static function create(array $container)
+    public static function create(array $container = array())
     {
         return new self($container);
     }
@@ -116,5 +118,466 @@ class _
         }
 
         return _::create($result);
+    }
+
+    /**
+     * Calls the given callback once for each element in the container, passing
+     * that element as the argument.
+     *
+     * @param Callback
+     */
+    public function each(Callable $callback)
+    {
+        for ($i = 0; $i < count($this->container); $i++) {
+            $callback($this->container[$i], $i, $this->container);
+        }
+    }
+
+    /**
+     * Passes each entry in the container to the given callback, returning the
+     * first element for which callback is not `false`. If no entry matches,
+     * returns `null`.
+     *
+     * @param Callable
+     *
+     * @return mixed
+     */
+    public function find(Callable $callback)
+    {
+        foreach ($this->container as $element) {
+            if ($callback($element) !== false) {
+                return $element;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Returns the first `n` elements in the container.
+     *
+     * @param integer
+     *
+     * @return _
+     */
+    public function first($n)
+    {
+        return static::create(array_slice($this->container, 0, $n));
+    }
+
+    /**
+     * Returns a new, one-dimensional array that is a recursive flattening of
+     * the container.
+     *
+     * @return _
+     */
+    public function flatten()
+    {
+        $r = array();
+
+        array_walk_recursive($this->container, function($a) use (&$r) { $r[] = $a; });
+
+        return static::create($r);
+    }
+
+    /**
+     * Returns a new array with the concatenated results of invoking the
+     * callback once for every element in the container.
+     *
+     * @param Callable
+     *
+     * @return _
+     */
+    public function flatMap(Callable $callback)
+    {
+        $result = [];
+
+        foreach ($this->container as $element) {
+            $result = array_merge($result, $callback($element));
+        }
+
+        return static::create($result);
+    }
+
+    /**
+     * Groups the container by result of the given callback.
+     *
+     * @param Callable
+     *
+     * @return _
+     */
+    public function groupBy(Callable $callback)
+    {
+        $result = [];
+
+        foreach ($this->container as $element) {
+            $result[$callback($element)][] = $element;
+        }
+
+        return static::create($result);
+    }
+
+    /**
+     * @param mixed
+     *
+     * @return Boolean
+     */
+    public function has($element)
+    {
+        return in_array($element, $this->container);
+    }
+
+    /**
+     * @param mixed
+     *
+     * @return integer|null
+     */
+    public function indexOf($element)
+    {
+        return array_search($element, $this->container) ?: null;
+    }
+
+    /**
+     * Returns a string of all the container's elements joined with the provided
+     * separator string.
+     *
+     * @param string
+     *
+     * @return string
+     */
+    public function join($seperator)
+    {
+        return implode($seperator, $this->container);
+    }
+
+    /**
+     * Returns the last `n` elements from the container.
+     *
+     * @param integer
+     *
+     * @return _
+     */
+    public function last($n)
+    {
+        return static::create(array_reverse(array_slice(array_reverse($this->container), 0, $n)));
+    }
+
+    /**
+     * Invokes the given callback for each element in the container. Creates a
+     * new array containing the values returned by the block.
+     *
+     * If the given callback returns `null`, that element is skipped in the
+     * returned array.
+     *
+     * @param Callable
+     *
+     * @return _
+     */
+    public function map(Callable $callback)
+    {
+        return static::create(array_values(array_filter(array_map(function ($e) use ($callback) {
+            return $callback($e);
+        }, $this->container))));
+    }
+
+    /**
+     * Returns the element for which the given callback returns the largest
+     * integer.
+     *
+     * @param Callable
+     *
+     * @return mixed
+     */
+    public function max(Callable $callback)
+    {
+        $data = array_values($this->container);
+        $max  = [$callback($data[0]), $data[0]];
+
+        for ($i = 1; $i < count($data); $i++) {
+            if (($new = $callback($data[$i])) > $max[0]) {
+                $max[0] = $new;
+                $max[1] = $data[$i];
+            }
+        }
+
+        return $max[1];
+    }
+
+    /**
+     * Returns the element for which the given callback returns the smallest
+     * integer.
+     *
+     * @param Callable
+     *
+     * @return mixed
+     */
+    public function min(Callable $callback)
+    {
+        $data = array_values($this->container);
+        $min  = [$callback($data[0]), $data[0]];
+
+        for ($i = 1; $i < count($data); $i++) {
+            if (($new = $callback($data[$i])) < $min[0]) {
+                $min[0] = $new;
+                $min[1] = $data[$i];
+            }
+        }
+
+        return $min[1];
+    }
+
+    /**
+     * Test if the given callback returns `false` for each element in the
+     * container.
+     *
+     * @param Callable
+     *
+     * @return Boolean
+     */
+    public function none(Callable $callback)
+    {
+        foreach ($this->container as $element) {
+            if ($callback($element)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Partitions the container into two arrays based on the boolean return
+     * value of the given block.
+     *
+     * @param Callback
+     *
+     * @return _
+     */
+    public function partition(Callable $callback)
+    {
+        $result = [];
+
+        foreach ($this->container as $element) {
+            $result[!((bool) $callback($element))][] = $element;
+        }
+
+        return $result;
+    }
+
+    /**
+     * Returns a new array that is the result of retrieving the given property
+     * path on each element in the receiver.
+     *
+     * @param string
+     *
+     * @return _
+     */
+    public function pluck($path)
+    {
+        $accessor = PropertyAccess::createPropertyAccessor();
+
+        return static::create(array_values(array_filter(array_map(function ($e) use ($path, $accessor) {
+            try {
+                return $accessor->getValue($e, $path);
+            } catch (\Exception $e) {
+                return null;
+            }
+        }, $this->container))));
+    }
+
+    /**
+     * Reduces the container to a single value.
+     *
+     * @param Callback
+     *
+     * @return mixed
+     */
+    public function reduce(Callable $callback)
+    {
+        return array_reduce($this->container, function ($m, $e) use ($callback) {
+            return $callback($m, $e);
+        });
+    }
+
+    /**
+     * Returns a new array containing all elements for which the given callback
+     * returns `false`.
+     *
+     * @param Callable
+     *
+     * @return _
+     */
+    public function reject(Callable $callback)
+    {
+        return static::create(array_values(array_filter($this->container, function ($e) use ($callback) {
+            return $callback($e) === false;
+        })));
+    }
+
+    /**
+     * Returns a new array that is the container, reversed.
+     *
+     * @return _
+     */
+    public function reverse()
+    {
+        return static::create(array_reverse($this->container));
+    }
+
+    /**
+     * Returns a new array containing all elements for which the given block
+     * returns `true`.
+     *
+     * @param Callable
+     *
+     * @return _
+     */
+    public function select(Callable $callback)
+    {
+        return static::create(array_values(array_filter($this->container, function ($e) use ($callback) {
+            return $callback($e) === true;
+        })));
+    }
+
+    /**
+     * Skips the first `n` elements and returns the rest of the array.
+     *
+     * @param integer
+     *
+     * @return _
+     */
+    public function skip($n)
+    {
+        return static::create(array_slice($this->container, $n));
+    }
+
+    /**
+     * Returns a subarray consisting of the given number of elements from the
+     * given starting index.
+     *
+     * @param integer
+     * @param integer
+     *
+     * @return _
+     */
+    public function slice($offset, $length)
+    {
+        return static::create(array_values(array_slice($this->container, $offset, $length)));
+    }
+
+    /**
+     * Snips the end off the array. Returns the container _without_ the last `n`
+     * elements.
+     *
+     * @param integer
+     *
+     * @return _
+     */
+    public function snip($n)
+    {
+        return static::create(array_slice($this->container, 0, count($this->container) - $n));
+    }
+
+    /**
+     * Assumes that the container is an array of arrays and transposes the rows
+     * and columns.
+     *
+     * @return _
+     */
+    public function transpose()
+    {
+        $result = $this->container;
+        array_unshift($result, null);
+
+        return static::create(call_user_func_array('array_map', $result));
+    }
+
+    /**
+     * Returns a new array by removing duplicate values in the container.
+     *
+     * @return _
+     */
+    public function uniq()
+    {
+        $result = [];
+
+        foreach ($this->container as $element) {
+            if (!in_array($element, $result)) {
+                $result[] = $element;
+            }
+        }
+
+        return static::create($result);
+    }
+
+    /**
+     * Returns a new array where objects in the given array are removed from
+     * the receiver.
+     *
+     * @param array
+     *
+     * @return _
+     */
+    public function without(array $filter)
+    {
+        $result = [];
+
+        foreach ($this->container as $element) {
+            if (!in_array($element, $filter)) {
+                $result[] = $element;
+            }
+        }
+
+        return static::create($result);
+    }
+
+    /**
+     * Treats receiver like a stack and removes the last object, returning it.
+     *
+     * @return mixed
+     */
+    public function pop()
+    {
+        return array_pop($this->container);
+    }
+
+    /**
+     * Treats container like a stack and adds the given object to the end of
+     * the container.
+     *
+     * @param mixed
+     *
+     * @return self
+     */
+    public function push($element)
+    {
+        $this->container[] = $element;
+
+        return $this;
+    }
+
+    /**
+     * Removes the container's first object and returns it.
+     *
+     * @return mixed
+     */
+    public function shift()
+    {
+        return array_shift($this->container);
+    }
+
+    /**
+     * Inserts the given object at the front of container, moving all other
+     * objects in the container up one index.
+     *
+     * @param mixed
+     *
+     * @return self
+     */
+    public function unshift($element)
+    {
+        array_unshift($this->container, $element);
+
+        return $this;
     }
 }
--- tests/_Test.php
+++ tests/_Test.php
@@ -42,4 +42,216 @@ class _Test extends \PHPUnit_Framework_TestCase
         $this->assertEquals([1 => 2, 3 => 4], _::create([[1, 2], [3, 4]])->dict()->toArray());
         $this->assertEquals([1 => 2, 3 => 4], _::create([1, 2, 3, 4])->chunk(2)->dict()->toArray());
     }
+
+    public function testEach()
+    {
+        $count = 0;
+
+        _::create([1, 2, 3, 4])->each(function ($n) use (&$count) {
+            $count++;
+        });
+
+        $this->assertEquals(4, $count);
+    }
+
+    public function testFind()
+    {
+        $this->assertEquals(3, _::create([1, 2, 3, 4])->find(function ($n) {
+            return $n > 2;
+        }));
+
+        $this->assertNull(_::create([1, 2, 3, 4])->find(function ($n) {
+            return $n < 0;
+        }));
+    }
+
+    public function testFirst()
+    {
+        $this->assertEquals([1, 2], _::create([1, 2, 3, 4])->first(2)->toArray());
+    }
+
+    public function testFlatten()
+    {
+        $this->assertEquals([1, 2, 3, 4], _::create([1, [2], [3, [4]]])->flatten()->toArray());
+    }
+
+    public function testFlatMap()
+    {
+        $this->assertEquals([1, 1, 2, 2, 3, 3, 4, 4], _::create([1, 2, 3, 4])->flatMap(function ($n) {
+            return [$n, $n];
+        })->toArray());
+
+        $this->assertEquals([1, [1], 2, [2], 3, [3], 4, [4]], _::create([1, 2, 3, 4])->flatMap(function ($n) {
+            return [$n, [$n]];
+        })->toArray());
+    }
+
+    public function testGroupBy()
+    {
+        $this->assertEquals(['f' => ['foo'], 'b' => ['bar', 'baz']], _::create(['foo', 'bar', 'baz'])->groupBy(function ($s) {
+            return $s[0];
+        })->toArray());
+    }
+
+    public function testHas()
+    {
+        $this->assertTrue(_::create([1, 2, 3, 4])->has(2));
+        $this->assertFalse(_::create([1, 2, 3, 4])->has(0));
+    }
+
+    public function testIndexOf()
+    {
+        $this->assertEquals(2, _::create([1, 2, 3, 4])->indexOf(3));
+        $this->assertEquals('foo', _::create(['foo' => 'bar', 'bar' => 'baz'])->indexOf('bar'));
+        $this->assertNull(_::create([1, 2, 3, 4])->indexOf(0));
+    }
+
+    public function testJoin()
+    {
+        $this->assertEquals('1234', _::create([1, 2, 3, 4])->join(''));
+        $this->assertEquals('1,2,3,4', _::create([1, 2, 3, 4])->join(','));
+    }
+
+    public function testLast()
+    {
+        $this->assertEquals([3, 4], _::create([1, 2, 3, 4])->last(2)->toArray());
+    }
+
+    public function testMap()
+    {
+        $this->assertEquals([1, 4, 9, 16], _::create([1, 2, 3, 4])->map(function ($n) {
+            return $n * $n;
+        })->toArray());
+
+        $this->assertEquals([1, 9, 16], _::create([1, 2, 3, 4])->map(function ($n) {
+            return $n == 2 ? null : $n * $n;
+        })->toArray());
+    }
+
+    public function testMax()
+    {
+        $data = ['1', 'two', 'three'];
+        $this->assertEquals('three', _::create($data)->max(function ($s) {
+            return strlen($s);
+        }));
+    }
+
+    public function testMin()
+    {
+        $data = ['tree', 'two', '1'];
+        $this->assertEquals('1', _::create($data)->min(function ($s) {
+            return strlen($s);
+        }));
+    }
+
+    public function testNone()
+    {
+        $this->assertTrue(_::create([1, 2, 3, 4])->none(function ($n) {
+            return $n > 4;
+        }));
+        $this->assertFalse(_::create([1, 2, 3, 4])->none(function ($n) {
+            return $n > 0;
+        }));
+    }
+
+    public function testPartition()
+    {
+        $this->assertEquals([['A', 'AA'], ['B', 'C']], _::create(['A', 'B', 'C', 'AA'])->partition(function ($s) {
+            return $s[0] == 'A';
+        }));
+    }
+
+    public function testPluck()
+    {
+        $users = [
+            (object) [ 'foo' => 'bar' ],
+            (object) [ 'username' => 'bob' ],
+            (object) [ 'username' => 'alice' ],
+        ];
+
+        $this->assertEquals(['bob', 'alice'], _::create($users)->pluck('username')->toArray());
+    }
+
+    public function testReduce()
+    {
+        $this->assertEquals(10, _::create([1, 2, 3, 4])->reduce(function ($memo, $n) {
+            return $memo + $n;
+        }));
+    }
+
+    public function testReject()
+    {
+        $this->assertEquals([1, 3], _::create([1, 2, 3, 4])->reject(function ($n) {
+            return ($n % 2) == 0;
+        })->toArray());
+    }
+
+    public function testReverse()
+    {
+        $this->assertEquals([4, 3, 2, 1], _::create([1, 2, 3, 4])->reverse()->toArray());
+    }
+
+    public function testSelect()
+    {
+        $this->assertEquals([2, 4], _::create([1, 2, 3, 4])->select(function ($n) {
+            return ($n % 2) == 0;
+        })->toArray());
+    }
+
+    public function testSkip()
+    {
+        $this->assertEquals([3, 4, 5, 6], _::create([1, 2, 3, 4, 5, 6])->skip(2)->toArray());
+    }
+
+    public function testSlice()
+    {
+        $this->assertEquals([2, 3], _::create([1, 2, 3, 4])->slice(1, 2)->toArray());
+    }
+
+    public function testSnip()
+    {
+        $this->assertEquals([1, 2], _::create([1, 2, 3, 4])->snip(2)->toArray());
+    }
+
+    public function testTranspose()
+    {
+        $this->assertEquals([[1, 4], [2, 5], [3, 6]], _::create([[1, 2, 3], [4, 5, 6]])->transpose()->toArray());
+    }
+
+    public function testUniq()
+    {
+        $this->assertEquals([1, 2, 3], _::create([1, 2, 3, 1, 2, 3])->uniq()->toArray());
+    }
+
+    public function testWithout()
+    {
+        $this->assertEquals([1, 2, 3], _::create([1, 2, 3, 4])->without([4])->toArray());
+        $this->assertEquals([2, 3], _::create([1, 2, 3, 4])->without([1, 4])->toArray());
+    }
+
+    public function testPop()
+    {
+        $_ = _::create([1, 2, 3, 4]);
+
+        $this->assertEquals(4, $_->pop());
+        $this->assertEquals([1, 2, 3], $_->toArray());
+    }
+
+    public function testPush()
+    {
+        $this->assertEquals([1, 2, 3], _::create()->push(1)->push(2)->push(3)->toArray());
+    }
+
+    public function testShift()
+    {
+        $_ = _::create([1, 2, 3]);
+
+        $this->assertEquals(1, $_->shift());
+        $this->assertEquals([2, 3], $_->toArray());
+    }
+
+    public function testUnshift()
+    {
+        $this->assertEquals([1, 2, 3], _::create()->unshift(3)->unshift(2)->unshift(1)->toArray());
+    }
 }
